fe3700da05d7bc2a87b6c5da948aa27184988d38
NovemberFoxtrot <november@fox.trot>
2013-08-23 01:03:57 -0700

    minor cleanup

--- winston.go
+++ winston.go
@@ -12,15 +12,7 @@ import (
 	"strings"
 )
 
-type BiCluster struct {
-	left     int
-	right    int
-	vec      float64
-	id       int
-	distance float64
-}
-
-func checkerror(err error) {
+func CheckError(err error) {
 	if err != nil {
 		log.Fatal("[ERROR]", err)
 	}
@@ -28,6 +20,7 @@ func checkerror(err error) {
 
 type Winston struct {
 	Text  string
+	SafeText  string
 	Grams []string
 	Freq  map[string]int
 }
@@ -92,20 +85,20 @@ func (w1 *Winston) Pearson(w2 *Winston) float64 {
 
 func (w *Winston) CleanText() {
 	asciiregexp, err := regexp.Compile("[^A-Za-z ]+")
-	checkerror(err)
+	CheckError(err)
 
 	tagregexp, err := regexp.Compile("<[^>]+>")
-	checkerror(err)
+	CheckError(err)
 
 	spaceregexp, err := regexp.Compile("[ ]+")
-	checkerror(err)
+	CheckError(err)
 
-	w.Text = tagregexp.ReplaceAllString(w.Text, " ")
-	w.Text = asciiregexp.ReplaceAllString(w.Text, " ")
-	w.Text = spaceregexp.ReplaceAllString(w.Text, " ")
-	w.Text = strings.Trim(w.Text, "")
-	w.Text = strings.ToLower(w.Text)
-	w.Text = strings.TrimSpace(w.Text)
+	w.SafeText = tagregexp.ReplaceAllString(w.Text, " ")
+	w.SafeText = asciiregexp.ReplaceAllString(w.SafeText, " ")
+	w.SafeText = spaceregexp.ReplaceAllString(w.SafeText, " ")
+	w.SafeText = strings.Trim(w.SafeText, "")
+	w.SafeText = strings.ToLower(w.SafeText)
+	w.SafeText = strings.TrimSpace(w.SafeText)
 }
 
 func (w *Winston) CalcGrams() {
@@ -118,7 +111,7 @@ func (w *Winston) CalcGrams() {
 		w.Freq[gram] += 1
 	}
 
-	fmt.Println(w.Freq)
+	fmt.Println(w.SafeText)
 }
 
 func (w *Winston) FetchUrl(theurl string) {
@@ -126,7 +119,7 @@ func (w *Winston) FetchUrl(theurl string) {
 
 	if proxy := os.Getenv("http_proxy"); proxy != `` {
 		proxyUrl, err := url.Parse(proxy)
-		checkerror(err)
+		CheckError(err)
 
 		client = &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
 	} else {
@@ -134,52 +127,15 @@ func (w *Winston) FetchUrl(theurl string) {
 	}
 
 	req, err := http.NewRequest(`GET`, theurl, nil)
-	checkerror(err)
+	CheckError(err)
 
 	resp, err := client.Do(req)
-	checkerror(err)
+	CheckError(err)
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	checkerror(err)
+	CheckError(err)
 
 	w.Text = string(body)
 }
-/*
-func main() {
-	winstons := make([]Winston, 0)
-
-	for _, url := range os.Args[1:] {
-		var w Winston
-		w.FetchUrl(url)
-		w.CalcGrams()
-		fmt.Println(len(w.Text), len(w.Grams), len(w.Freq))
-		winstons = append(winstons, w)
-	}
-
-	if len(winstons) < 1 {
-		os.Exit(1)
-	}
-
-	biclusters := make([]BiCluster, 0)
-
-	for n := 0; n < len(winstons); n++ {
-		closestm := -1
-		closestdis := -1.0
-		for m := n + 1; m < len(winstons); m++ {
-			dis := winstons[n].Pearson(&winstons[m])
-			if closestdis < dis {
-				closestdis = dis
-				closestm = m
-			}
-
-		}
-
-		bic := BiCluster{id: n, left: n, right: closestm, distance: closestdis}
-		biclusters = append(biclusters, bic)
-	}
-
-	fmt.Println(biclusters)
-}
-*/
